ef631cd71ae173426d61f989ff45272c144fa4e4
fryzel <u1070220@utah.edu>
2021-04-22 21:58:08 -0600

    Correct Versioning

--- MontageServer/Migrations/MontageDBContextModelSnapshot.cs
+++ MontageServer/Migrations/MontageDBContextModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace MontageServer.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.2")
+                .HasAnnotation("ProductVersion", "3.1.12")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
