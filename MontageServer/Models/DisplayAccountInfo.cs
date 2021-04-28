904efdd31c3d2799a18df82986c19ee712be8721
Gavin Monson <gnosnow@msn.com>
2021-04-28 13:00:22 -0600

    Adding pretty json view and account showing you have been logged in

---
+++ MontageServer/Models/DisplayAccountInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MontageServer.Models
+{
+    public class DisplayAccountInfo
+    {
+        public string ClipId { get; set; }
+        public string FootagePath { get; set; }
+        public DisplayClipInformation displayClipInformation { get; set; }
+    }
+
+    public class DisplayClipInformation 
+    {
+        public string Transcript { get; set; }
+
+        public string Topics { get; set; }
+
+    }
+}
